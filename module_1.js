f0609237a7b4e7442ec0d067aed283cab6873ee7
AlinaGay <gayfullina.a@gmail.com>
2018-08-20 17:31:13 +0300

    Initial commit

---
+++ module_1.js
@@ -0,0 +1,103 @@
+//Number
+
+//1
+console.log(128);
+
+//2
+console.log(7.56);
+
+//3
+console.log(2 + 2);
+
+//4
+console.log(+2);
+
+//5
+console.log(2 + 2 * 2);
+
+//6
+console.log((2 + 2) * 2);
+
+//7
+console.log((1 / 3).toFixed(2));
+
+//String type
+
+//8
+console.log("Hello, world!");
+console.log('Hello, world!');
+
+//9
+//concatenation
+
+console.log('Hello '+'my '+'world '+'!');
+
+//10
+//lenght
+
+console.log('hello'.length);
+
+//11
+//Upper case
+
+console.log('hello'.toUpperCase());
+
+//12
+//unicode
+
+console.log('Это \u2014\x20 просто строка');
+
+//13
+//screening
+
+console.log('Hello, \n\tworld!');
+console.log('Hello \'my\' world!');
+
+//Boolean
+
+//Comparison Operators
+
+//14
+//Equality
+console.log(5==5);
+console.log((5=='5'));
+
+//15
+//Identity
+console.log(5===5);
+console.log(5==='5');
+
+//16
+//Inequality
+console.log(5!=5);
+
+//17
+//Non-identity
+console.log(5!==6);
+
+//18
+//other operators
+console.log(3 > 4);
+console.log(3 >= 4);
+console.log(3 < 4);
+console.log(3 <= 4);
+
+//Logical Operators
+
+//19
+console.log(!true);
+console.log(!false);
+console.log(!5);
+console.log(!0);
+
+//20
+console.log(3 && 4);
+console.log(3 && 0);
+console.log("" && 2);
+console.log("" && 0);
+
+//21
+console.log(3 || 4);
+console.log(3 || 0);
+console.log("" || 2);
+console.log("" || 0);
